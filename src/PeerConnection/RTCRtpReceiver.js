cc4ab5a0adf13cda44e5037929ce2ed65deed991
SUZUKI Tetsuya <szktty@shiguredo.jp>
2018-11-02 22:43:44 +0900

    プロパティを定義する

--- src/PeerConnection/RTCRtpReceiver.js
+++ src/PeerConnection/RTCRtpReceiver.js
@@ -1,9 +1,24 @@
 // @flow
 
+import RTCMediaStreamTrack from '../MediaStream/RTCMediaStreamTrack';
+import { RTCRtpParameters } from './RTCRtpParameters';
 import type { ValueTag } from './RTCPeerConnection';
 
 export default class RTCRtpReceiver {
 
+  receiverId: String;
+  parameters: RTCRtpParameters;
+  track: RTCMediaStreamTrack;
+
   _valueTag: ValueTag;
+
+  /**
+   * @package
+   */
+  constructor(info: Object) {
+    this.receiverId = info.receiverId;
+    this.parameters = new RTCRtpParameters(info.parameters);
+    this.track = new RTCMediaStreamTrack(info.track);
+  }
 
 }
