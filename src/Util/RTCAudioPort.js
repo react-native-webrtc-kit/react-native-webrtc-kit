b730034d1e6bcb412d627339a6379d5591138568
SUZUKI Tetsuya <szktty@shiguredo.jp>
2019-01-31 16:45:41 +0900

    ドキュメントにバージョンを書く

--- src/Util/RTCAudioPort.js
+++ src/Util/RTCAudioPort.js
@@ -7,23 +7,29 @@ import logger from './RTCLogger';
  * - `'speaker'` - スピーカー
  * - `'unknown'` - 不明
  * @typedef {string} RTCAudioPort
- * @version 2.0.0
+ * 
+ * @since 2.1.0
  */
 export type RTCAudioPort =
   | 'none'
   | 'speaker'
   | 'unknown'
-/*
+
+/**
  * オーディオの出力元を取得します。
  * @return {Promise<RTCAudioPort>}
+ * 
+ * @since 2.1.0
  */
 export function getAudioPort(): Promise<RTCAudioPort> {
   return WebRTC.getAudioPort();
 }
 
-/*
+/**
  * オーディオの出力先を指定します。
- * @param {isSpeaker} Audio Port を Speaker にするか None にするかの指定 
+ * @param {boolean} Audio Port を Speaker にするか None にするかの指定 
+ * 
+ * @since 2.1.0
  */
 export function setAudioPort(isSpeaker: boolean): Promise<void> {
   logger.log("# audio route change");
