162b454ea288c2297aa2c831383e7eec8b9f6dbe
akisute <akisutesama@gmail.com>
2019-05-09 12:02:49 +0900

    Fix: onTrack()でクラッシュする問題を修正

--- android/src/main/java/com/reactlibrary/WebRTCPeerConnectionObserver.java
+++ android/src/main/java/com/reactlibrary/WebRTCPeerConnectionObserver.java
@@ -212,14 +212,6 @@ final class WebRTCPeerConnectionObserver implements PeerConnection.Observer {
         [self removeTransceiverForKey: transceiver.valueTag];
         [WebRTCValueManager removeValueTagForObject: transceiver];
          */
-        // TODO: 以下の例外を修正する。これは他に誰もいないchannelに対して最初の一人目のsubscriberとして接続したときに発生する。
-        //       他に誰もいないからRtpSenderのstreamIdが空ということらしい。この場合どのような挙動が望ましいか調べて修正すること。
-        /*
-       　W/System.err: java.lang.IllegalStateException
-       　W/System.err:     at com.reactlibrary.WebRTCConverter.rtpSenderJsonValue(WebRTCConverter.java:125)
-       　W/System.err:     at com.reactlibrary.WebRTCConverter.rtpTransceiverJsonValue(WebRTCConverter.java:190)
-       　W/System.err:     at com.reactlibrary.WebRTCPeerConnectionObserver.onTrack(WebRTCPeerConnectionObserver.java:233)
-        */
         if (peerConnectionPair == null) return;
         final WebRTCModule module = getModule();
         module.repository.transceivers.add(transceiver.getMid(), module.createNewValueTag(), transceiver);
@@ -235,6 +227,8 @@ final class WebRTCPeerConnectionObserver implements PeerConnection.Observer {
         if (receiverTrack != null) {
             module.repository.tracks.add(receiverTrack.id(), module.createNewValueTag(), receiverTrack);
         }
+        // XXX: 本来であればここで sender.streams() や receiver.streams() を使ってstreamIdsを取得し、repository.setStreamIds(...)とする必要がある
+        //      しかしながら現在libwebrtcに sender.streams() や receiver.streams() の実装がないため実現不能、fxxk
 
         final WritableMap params = Arguments.createMap();
         params.putString("valueTag", peerConnectionPair.first);
